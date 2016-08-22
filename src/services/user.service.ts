bc25398368daa2577153e2b0fb34aafeae0ef17e
Michael Wild <mwild95@live.co.uk>
2016-08-22 22:06:48 +0100

    Header template and User Service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,8 +2,32 @@ import { Injectable } from '@angular/core';
 import { LoginService } from './login.service'; 
 
 @Injectable()
-class UserService {
-	constructor( loginService: LoginService ) {
-		loginService.login();
+export class UserService {
+
+	loggedIn : boolean = false;
+	username : string = "Test";
+	password : string = "";
+
+	constructor( ) {
+		
+	}
+
+	login ( username : string, password : string ) {
+		//this.loggedIn = loginService.login( username, password );
+
+		return loggedIn;
+	}
+
+	logout ( ) {
+		this.loggedIn = false;
+	}
+
+	getUsername ( ) {
+		return this.username;
+	}
+
+	//need to return a user token rather than the password
+	getUserToken ( ) {
+		return this.password;
 	}
 }
